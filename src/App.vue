5bc96c68dde5be3a59e029b0e30fbada20c7ab93
lqh175643 <1756431742@qq.com>
2020-11-29 14:34:32 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,5 @@ export default {
 </script>
 
 <style>
-  @import "assets/css/base"
+  @import "./assets/css/base"
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
     configureWebpack: {
         resolve: {
             extensions: [],
-            alias: {
-                'assets': '@/assets',
-                'common': '@/common',
-                'components': '@/components',
-                'network': '@/network',
-                'views': '@/views'
-            }
+            // alias: {
+            //     'assets': '@/assets',
+            //     'common': '@/common',
+            //     'components': '@/components',
+            //     'network': '@/network',
+            //     'views': '@/views'
+            // }
         }
     }
 }
